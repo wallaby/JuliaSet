a03c12539af199a21419cb9db247ee75de710246
Toth Balazs <toth.balazs@gmx.com>
2020-03-30 10:45:50 +0100

    Initial commit

---
+++ app/src/main/java/onl/toth/dev/app/julia/MainActivity.kt
@@ -0,0 +1,11 @@
+package onl.toth.dev.app.julia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
